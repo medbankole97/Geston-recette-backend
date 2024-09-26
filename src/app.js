e471d6484b18dcd65c215bf1137b284695bc7439
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-09-26 14:53:00 +0100

    Install cors in app

--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@ import router from './routes/index.js';
 import bodyParser from 'body-parser';
 // import pool from './config/db.js';
 import { config } from 'dotenv';
+import cors from 'cors';
 config();
 
 const app = express();
 app.use(bodyParser.json());
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+};
+app.use(cors(corsOptions()));
 app.use(router);
 
 const PORT = process.env.NODE_DOCKER_PORT || 3021;
