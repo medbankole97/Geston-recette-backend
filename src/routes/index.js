c7129f2552b3a29e7a3a81ff671cf2bed049afde
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-09-27 12:16:44 +0100

    Upadated routes access

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,66 @@
 import express from 'express';
 import RecipeController from '../controllers/RecipeController.js';
+import CategoryController from '../controllers/CategoryController.js';
+
 import {
-  addRequestValidator,
-  deleteRequestValidator,
-  updateRequestValidator,
+  addRequestValidatorRecipe,
+  addRequestValidatorCategory,
+  updateRequestValidatorRecipe,
+  updateRequestValidatorCategory,
+  deleteRequestValidatorRecipe,
+  deleteRequestValidatorCategory,
 } from '../validators/RecetteValidator.js';
 
 const router = express.Router();
 
-router.get('/recipes', RecipeController.getAllRecipes);
+//recipes routes
+router.get('/recipes', RecipeController.recipes);
 
-router.post('/recipes', addRequestValidator, RecipeController.createRecipe);
+router.post('/recipes', addRequestValidatorRecipe, RecipeController.store);
 
 router.delete(
   '/recipes/:id',
-  deleteRequestValidator,
-  RecipeController.deleteRecipe
+  deleteRequestValidatorRecipe,
+  RecipeController.destroy
 );
 
 router.put(
   '/recipes/:id',
-  updateRequestValidator,
-  RecipeController.updateRecipe
+  updateRequestValidatorRecipe,
+  RecipeController.update
 );
 
-router.get('/recipes/:id', deleteRequestValidator, RecipeController.getByID);
+router.get(
+  '/recipes/:id',
+  deleteRequestValidatorRecipe,
+  RecipeController.getById
+);
+
+//categories route
+router.get('/categories', CategoryController.categories);
+
+router.post(
+  '/categories',
+  addRequestValidatorCategory,
+  CategoryController.store
+);
+
+router.delete(
+  '/categories/:id',
+  deleteRequestValidatorCategory,
+  CategoryController.destroy
+);
+
+router.put(
+  '/categories/:id',
+  updateRequestValidatorCategory,
+  CategoryController.update
+);
+
+router.get(
+  '/categories/:id',
+  deleteRequestValidatorCategory,
+  CategoryController.getById
+);
 
 export default router;
